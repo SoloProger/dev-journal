a7ef0cf5fe962ab13de712b5f8c7d69d8e0f1901
SoloProger <solo.proger@gmail.com>
2023-03-14 17:16:06 +0300

    CLIENT: Добавил глобальный store

---
+++ client/src/shared/hooks/useApi.js
@@ -0,0 +1,3 @@
+export default function useApi(Class) {
+  return new Class();
+}
---
+++ client/src/shared/store/blog/postSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import PostApi from "../../api/post.api";
+
+const initialState = {
+  posts: [],
+};
+
+const postApi = new PostApi();
+
+export const loadPost = createAsyncThunk("post/load", async () => {
+  const posts = await postApi.getPosts();
+  return posts;
+});
+
+export const addNewPost = createAsyncThunk(
+  "post/create",
+  async (payload, thunkApi) => {
+    const post = await postApi.createPost(payload);
+    thunkApi.dispatch(addPost(payload));
+    return post;
+  }
+);
+
+export const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    addPost(state, action) {
+      state.posts.push(action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(loadPost.fulfilled, (state, action) => {
+      state.posts = action.payload;
+    });
+  },
+});
+
+const { addPost } = postSlice.actions;
+
+export default postSlice.reducer;
