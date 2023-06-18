a2d5cca818920a1998438ced0e799d7cb4fb3393
ITSerafim <aserafimow@gmail.com>
2023-06-18 22:06:49 +0300

    Initial settings for project

--- app/src/sql/create_users_table.sql
+++
@@ -1,9 +0,0 @@
-create table users (
-    id int primary key auto_increment,
-    name varchar(55) not null,
-    surname varchar(155) not null,
-    login varchar(50) not null,
-    password text not null,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
-);
